c49808a17d8e4128043a3e939b3073854646b050
hustle-dev <dlwoabsdk@gmail.com>
2023-03-27 23:21:02 +0900

    feat: header 컴포넌트 스타일링 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Logo from '../../images/logo.svg';
-import Rss from '../../images/rss.svg';
-import DarkMode from '../../images/darkmode.svg';
+import Logo from '@images/logo.svg';
+import Rss from '@images/rss.svg';
+import DarkMode from '@images/darkmode.svg';
 import * as styles from './Header.module.css';
 import { Link } from 'gatsby';
 
@@ -18,8 +18,13 @@ export const Header = () => {
         </h1>
       </Link>
       <div className={styles.headerButtons}>
-        <Rss className={styles.icon} />
-        <DarkMode className={styles.icon} />
+        {/* TODO: RSS 피드 달면 링크 수정하기 */}
+        <Link to="/">
+          <Rss className={styles.icon} />
+        </Link>
+        <button className={styles.iconButton}>
+          <DarkMode className={styles.icon} />
+        </button>
       </div>
     </header>
   );
