e5f734c97b24da98ef6a28c78899189e0a70a839
hustle-dev <dlwoabsdk@gmail.com>
2023-03-30 15:42:41 +0900

    conf: hook alias path 설정

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -8,6 +8,7 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({ act
         '@components': path.resolve(__dirname, 'src/components'),
         '@images': path.resolve(__dirname, 'src/images'),
         '@styles': path.resolve(__dirname, 'src/styles'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
       },
     },
   });
