38f08976f9513eac02c3a8502fb47f3231ec1f5e
hustle-dev <dlwoabsdk@gmail.com>
2023-04-04 01:25:26 +0900

    feat: header 스타일링 및 progressbar 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '@images/logo.svg';
 import Rss from '@images/rss.svg';
 import DarkMode from '@images/darkmode.svg';
@@ -6,26 +6,65 @@ import * as styles from './Header.module.css';
 import { Link } from 'gatsby';
 import { useTheme } from '@hooks';
 
-export const Header = () => {
+type HeaderProps = {
+  pathname: string;
+};
+
+export const Header = ({ pathname }: HeaderProps) => {
   const toggleHandler = useTheme();
+  const [progressWidth, setProgressWidth] = useState<number>(0);
+
+  useEffect(() => {
+    if (pathname === '/') return;
+
+    let animationFrameId: number | null = null;
+
+    const handleScroll = () => {
+      const updateProgress = () => {
+        const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrollPosition = window.scrollY;
+        const scrollProgress = (scrollPosition / scrollHeight) * 100;
+        setProgressWidth(scrollProgress);
+        animationFrameId = null;
+      };
+
+      if (!animationFrameId) {
+        animationFrameId = window.requestAnimationFrame(updateProgress);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      if (animationFrameId) {
+        window.cancelAnimationFrame(animationFrameId);
+      }
+    };
+  }, [pathname]);
+
+  const isMainPage = pathname === '/';
 
   return (
-    <header className={styles.header}>
-      <Link to="/" className={styles.headingLink}>
-        <h1 className={styles.headingWrapper}>
-          <Logo />
-          <span className={styles.heading}>Hustle-devlog</span>
-        </h1>
-      </Link>
-      <div className={styles.headerButtons}>
-        {/* TODO: RSS 피드 달면 링크 수정하기 */}
-        <Link to="/">
-          <Rss className={styles.icon} />
+    <header className={`${styles.header} ${!isMainPage ? styles.fixed : ''}`}>
+      <div className={styles.wrapper}>
+        <Link to="/" className={styles.headingLink}>
+          <h1 className={styles.headingWrapper}>
+            <Logo />
+            <span className={styles.heading}>Hustle-devlog</span>
+          </h1>
         </Link>
-        <button className={styles.iconButton} onClick={toggleHandler} tabIndex={0}>
-          <DarkMode className={styles.icon} />
-        </button>
+        <div className={styles.headerButtons}>
+          {/* TODO: RSS 피드 달면 링크 수정하기 */}
+          <Link to="/">
+            <Rss className={styles.icon} />
+          </Link>
+          <button className={styles.iconButton} onClick={toggleHandler} tabIndex={0}>
+            <DarkMode className={styles.icon} />
+          </button>
+        </div>
       </div>
+      {pathname !== '/' && <div className={styles.progressBar} style={{ width: `${progressWidth}%` }} />}
     </header>
   );
 };
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,14 +1,15 @@
 import React, { type ReactNode } from 'react';
 import { Footer, Header } from '@components';
+import { PageProps } from 'gatsby';
 
 type LayoutProps = {
   children: ReactNode;
 };
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ location, children }: Omit<PageProps, 'children'> & LayoutProps) => {
   return (
     <>
-      <Header />
+      <Header pathname={location.pathname} />
       {children}
       <Footer />
     </>
