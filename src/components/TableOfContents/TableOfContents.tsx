7ba6fdc4f29edf7956f81323d1e8cc6dccc4b891
hustle-dev <dlwoabsdk@gmail.com>
2023-04-05 11:27:29 +0900

    feat: toc 스타일링 완성

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -1,11 +1,55 @@
 import { TYPO } from '@styles';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as styles from './TableOfContents.module.css';
+import Pen from '@images/pen.svg';
 
 type TableOfContentsProps = {
   html: string;
 };
 
 export const TableOfContents = ({ html }: TableOfContentsProps) => {
-  return <div style={TYPO.B7} className={styles.tableOfContents} dangerouslySetInnerHTML={{ __html: html }}></div>;
+  const tocRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const id = entry.target.id;
+            const tocItem = tocRef.current?.querySelector(`a[href="#${encodeURI(id)}"]`);
+
+            if (!tocItem) return;
+
+            const prevActiveItem = tocRef.current?.querySelector(`.${styles.active}`);
+            if (prevActiveItem) prevActiveItem.classList.remove(styles.active);
+
+            tocItem.classList.add(styles.active);
+          }
+        });
+      },
+      {
+        rootMargin: '0% 0% -90% 0%',
+        threshold: 0,
+      }
+    );
+
+    const headings = document.querySelectorAll('h1[id], h2[id], h3[id], h4[id]');
+    headings.forEach((heading) => observer.observe(heading));
+
+    return () => {
+      headings.forEach((heading) => observer.unobserve(heading));
+    };
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <Pen className={styles.pen} />
+      <div
+        ref={tocRef}
+        style={TYPO.B7}
+        className={styles.tableOfContents}
+        dangerouslySetInnerHTML={{ __html: html }}
+      ></div>
+    </div>
+  );
 };
