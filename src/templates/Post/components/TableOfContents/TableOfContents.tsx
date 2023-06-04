fc37ffbc52e3b624c94559411ec0aa9817fec70d
hustle-dev <dlwoabsdk@gmail.com>
2023-06-04 22:34:07 +0900

    feat: toc ui와 비즈니스 로직 분리

--- src/templates/Post/components/TableOfContents/TableOfContents.tsx
+++ src/templates/Post/components/TableOfContents/TableOfContents.tsx
@@ -1,55 +1,21 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import clsx from 'clsx'
 import Pen from '@/images/pen.svg'
 import * as styles from './TableOfContents.module.css'
 import * as typo from '@/styles/typography.module.css'
+import { useTocStyleObserver } from './hooks'
 
 type TableOfContentsProps = {
   html: string
 }
 
 export const TableOfContents = ({ html }: TableOfContentsProps) => {
-  const tocRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            const id = entry.target.id
-            const tocItem = tocRef.current?.querySelector(`a[href="#${encodeURI(id)}"]`)
-
-            if (!tocItem) return
-
-            const prevActiveItem = tocRef.current?.querySelector(`.${styles.active}`)
-            if (prevActiveItem) prevActiveItem.classList.remove(styles.active)
-
-            tocItem.classList.add(styles.active)
-          }
-        })
-      },
-      {
-        rootMargin: '0% 0% -90% 0%',
-        threshold: 0,
-      }
-    )
-
-    const headings = document.querySelectorAll('h1[id], h2[id], h3[id], h4[id]')
-    headings.forEach((heading) => observer.observe(heading))
-
-    return () => {
-      headings.forEach((heading) => observer.unobserve(heading))
-    }
-  }, [])
+  const { ref } = useTocStyleObserver()
 
   return (
     <div className={styles.wrapper}>
       <Pen className={styles.pen} />
-      <div
-        ref={tocRef}
-        className={clsx(styles.tableOfContents, typo.B7)}
-        dangerouslySetInnerHTML={{ __html: html }}
-      ></div>
+      <div ref={ref} className={clsx(styles.tableOfContents, typo.B7)} dangerouslySetInnerHTML={{ __html: html }}></div>
     </div>
   )
 }
---
+++ src/templates/Post/components/TableOfContents/hooks/useTocStyleObserver.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from 'react'
+import * as styles from '../TableOfContents.module.css'
+
+export const useTocStyleObserver = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const id = entry.target.id
+            const tocItem = ref.current?.querySelector(`a[href="#${encodeURI(id)}"]`)
+
+            if (!tocItem) return
+
+            const prevActiveItem = ref.current?.querySelector(`.${styles.active}`)
+            if (prevActiveItem) prevActiveItem.classList.remove(styles.active)
+
+            tocItem.classList.add(styles.active)
+          }
+        })
+      },
+      {
+        rootMargin: '0% 0% -90% 0%',
+        threshold: 0,
+      }
+    )
+
+    const headings = document.querySelectorAll('h2[id], h3[id]')
+    headings.forEach((heading) => observer.observe(heading))
+    return () => {
+      headings.forEach((heading) => observer.unobserve(heading))
+    }
+  }, [ref])
+
+  return { ref }
+}
