12e383d57373cd7a0aea0cde976d9542a5ce544e
hustle-dev <dlwoabsdk@gmail.com>
2023-06-04 11:47:15 +0900

    fix: build과정 SSR 해결을 위한 환경에 따른 조건 추가

--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useCallback, useContext, createContext } from 'react'
 import type { ReactNode } from 'react'
+import { isBrowser } from '@/utils'
 
 export enum Theme {
   LIGHT = 'light',
@@ -21,18 +22,20 @@ const ThemeContext = createContext<ThemeContextType>(defaultThemeContextValue)
 const isDarkMode = (theme: Theme) => theme === Theme.DARK
 
 const getInitialTheme = () => {
-  const localTheme = localStorage.getItem('theme') as Theme | null
+  const localTheme = window.localStorage.getItem('theme') as Theme | null
   const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? Theme.DARK : Theme.LIGHT
   return localTheme || prefersColorScheme
 }
 
 const setThemeAttribute = (theme: Theme) => {
+  if (!isBrowser) return
+
   document.documentElement.toggleAttribute('dark-mode', isDarkMode(theme))
   localStorage.setItem('theme', theme)
 }
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<Theme>(getInitialTheme())
+  const [theme, setTheme] = useState<Theme>(isBrowser ? getInitialTheme() : Theme.LIGHT)
   setThemeAttribute(theme)
 
   const toggleDarkMode = useCallback(() => {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './dom'
+export * from './env'
 export * from './guard'
 export * from './reactCSS'
 export * from './scroll'
