ed21416f73fcdcdadbc5b1d874275e3836458d50
hustle-dev <dlwoabsdk@gmail.com>
2023-06-04 22:59:46 +0900

    refactor: gatsby page 디렉터링 특성으로 인한 폴더 구조 분리

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,41 @@
+import type { HeadProps, PageProps } from 'gatsby'
+import React from 'react'
+import { PostList, RecentPost, TagList } from './components'
+import { usePostInfiniteScroll, useTag } from './hooks'
+import { FloatingButton, ProfileCard, Seo } from '@/components'
+import * as styles from './Home.module.css'
+
+const Home = ({ data, location: { pathname } }: PageProps<Queries.HomeQuery>) => {
+  const { nodes: allPosts, totalCount, group } = data.allMarkdownRemark
+  const recentPosts = allPosts.slice(0, 2)
+  const { tags, selectedTag, clickTag } = useTag(totalCount, group)
+  const { visiblePosts } = usePostInfiniteScroll(allPosts, selectedTag, totalCount)
+
+  return (
+    <main className={styles.main}>
+      <aside className={styles.aside}>
+        <ProfileCard pathname={pathname} />
+      </aside>
+      <div className={styles.shrinkSpace}></div>
+      <section className={styles.wrapper}>
+        <RecentPost posts={recentPosts} />
+        <hr className={styles.divider}></hr>
+        <TagList tags={tags} selectedTag={selectedTag} clickTag={clickTag} className={styles.tagList} />
+        <PostList posts={visiblePosts} className={styles.postList} />
+      </section>
+      <FloatingButton />
+    </main>
+  )
+}
+
+export default Home
+
+export const Head = ({ location: { pathname }, data: { site, file } }: HeadProps<Queries.HomeQuery>) => {
+  const seo = {
+    title: site?.siteMetadata.title,
+    description: site?.siteMetadata.description,
+    heroImage: file?.publicURL as string,
+  }
+
+  return <Seo title={seo.title} description={seo.description} heroImage={seo.heroImage} pathname={pathname}></Seo>
+}
---
+++ src/views/NotFound/NotFound.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import * as styles from './NotFound.module.css'
+
+const NotFoundPage = () => {
+  return (
+    <main className={styles.page}>
+      <h1 className={styles.heading}>Page not found</h1>
+      <p className={styles.paragraph}>
+        Sorry 😔, we couldn’t find what you were looking for.
+        <br />
+        {process.env.NODE_ENV === 'development' ? (
+          <>
+            <br />
+            Try creating a page in <code className={styles.code}>src/pages/</code>.
+            <br />
+          </>
+        ) : null}
+        <br />
+        <Link to="/">Go home</Link>.
+      </p>
+    </main>
+  )
+}
+
+export default NotFoundPage
+
+export const Head = () => <title>Not found</title>
