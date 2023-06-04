1810cb082500cf97ebaf2201b3664cc9c6278f41
hustle-dev <dlwoabsdk@gmail.com>
2023-06-04 20:42:28 +0900

    refactor: gatsby node api를 통해 페이지 생성

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,59 @@
 import type { CreateSchemaCustomizationArgs, CreateWebpackConfigArgs, GatsbyNode } from 'gatsby'
 import path from 'path'
 
+export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/templates/Post.tsx`)
+
+  const result = await graphql<Queries.PagesQuery>(`
+    query Pages {
+      allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+            id
+          }
+          previous {
+            frontmatter {
+              slug
+              title
+            }
+          }
+          next {
+            frontmatter {
+              slug
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  const posts = result.data?.allMarkdownRemark.edges
+
+  posts?.forEach(({ node, previous, next }) => {
+    createPage({
+      path: `/posts${node.frontmatter.slug}`,
+      component: blogPostTemplate,
+      context: {
+        id: node.id,
+        frontmatter__slug: node.frontmatter.slug,
+        previous: previous === null ? null : previous.frontmatter.slug,
+        previousTitle: previous === null ? null : previous.frontmatter.title,
+        next: next === null ? null : next.frontmatter.slug,
+        nextTitle: next === null ? null : next.frontmatter.title,
+      },
+    })
+  })
+}
+
 export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({ actions }: CreateWebpackConfigArgs) => {
   actions.setWebpackConfig({
     resolve: {
