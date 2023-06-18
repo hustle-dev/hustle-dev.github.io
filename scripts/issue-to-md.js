b50841d3a5b291fa99817d45ffa834640ec789dc
hustle-dev <dlwoabsdk@gmail.com>
2023-06-18 15:36:12 +0900

    feat: issue to md 파이프라인 생성

---
+++ scripts/issue-to-md.js
@@ -0,0 +1,49 @@
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+
+const downloadImage = async (url, localPath) => {
+  const response = await axios({
+    method: 'GET',
+    url,
+    responseType: 'stream',
+  })
+
+  const writer = fs.createWriteStream(localPath)
+  response.data.pipe(writer)
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', () => resolve(response.headers['content-type']))
+    writer.on('error', reject)
+  })
+}
+
+const processMarkdown = async (markdownFile) => {
+  let markdown = fs.readFileSync(markdownFile, 'utf8')
+  const imgRegex = /!\[.*?\]\((.*?)\)|<img.*?src="(.*?)"/g
+  const match = imgRegex.exec(markdown)
+
+  if (!match) {
+    console.error('No images found in markdown')
+    return
+  }
+
+  const imgUrl = match[1] || match[2]
+  const fileName = path.basename(new URL(imgUrl).pathname)
+  const localPath = path.join(path.dirname(markdownFile), fileName)
+
+  await downloadImage(imgUrl, localPath)
+
+  markdown = markdown.replace(match[0], '')
+
+  fs.writeFileSync(markdownFile, markdown, 'utf8')
+}
+
+const inputMarkdownFile = process.argv[2]
+
+if (!inputMarkdownFile) {
+  console.log('Usage: node index.js <inputMarkdownFile>')
+  process.exit(1)
+}
+
+processMarkdown(inputMarkdownFile)
