cbe9a0134020059cc31de139bc8843babfe5161c
hustle-dev <hustle-dev@webtoonscorp.com>
2023-11-07 00:03:12 +0900

    conf: gatsby sass 패키지 설치

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,17 +1,11 @@
-import '@/styles/reset.css'
-import '@/styles/global.css'
 import 'prismjs/themes/prism-tomorrow.css'
-import '@/styles/code.css'
+import '@/styles/index.scss'
 
-import type { WrapPageElementBrowserArgs } from 'gatsby'
+import type { WrapPageElementBrowserArgs, WrapRootElementBrowserArgs } from 'gatsby'
 
 import { ThemeProvider } from '@/contexts'
 
 import Layout from './src/layouts/Layout'
 
-// TODO: provider는 wrapRootElement에 적용하는게 좋을듯?
-export const wrapPageElement = ({ element, props }: WrapPageElementBrowserArgs) => (
-  <ThemeProvider>
-    <Layout {...props}>{element}</Layout>
-  </ThemeProvider>
-)
+export const wrapPageElement = ({ element, props }: WrapPageElementBrowserArgs) => <Layout {...props}>{element}</Layout>
+export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) => <ThemeProvider>{element}</ThemeProvider>
