9c9a015c161f903ae8498c2a542e8984e34b3638
hustle-dev <dlwoabsdk@gmail.com>
2023-11-25 20:58:08 +0900

    fix: 원활한 테마 공급을 위한 gatsby API 변경

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,11 +1,14 @@
 import 'prismjs/themes/prism-tomorrow.css'
 import '@/styles/index.scss'
 
-import type { WrapPageElementBrowserArgs, WrapRootElementBrowserArgs } from 'gatsby'
+import type { WrapPageElementBrowserArgs } from 'gatsby'
 
 import { ThemeProvider } from '@/contexts'
 
 import Layout from './src/layouts/Layout'
 
-export const wrapPageElement = ({ element, props }: WrapPageElementBrowserArgs) => <Layout {...props}>{element}</Layout>
-export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) => <ThemeProvider>{element}</ThemeProvider>
+export const wrapPageElement = ({ element, props }: WrapPageElementBrowserArgs) => (
+  <ThemeProvider>
+    <Layout {...props}>{element}</Layout>
+  </ThemeProvider>
+)
